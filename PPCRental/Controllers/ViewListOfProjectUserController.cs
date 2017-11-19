40b5ae234d6f6994b7f6487b74f4bed650779af9
PhuongTrang <phuongtrang1397@gmail.com>
2017-11-19 19:53:53 +0700

    view list of project

---
+++ PPCRental/Controllers/ViewListOfProjectUserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental.Models;
+
+namespace PPCRental.Controllers
+{
+    public class ViewListOfProjectUserController : Controller
+    {
+        K21T1_Team3Entities db = new K21T1_Team3Entities();
+        // GET: ViewListOfProjectUser
+        public ActionResult ViewListOfProjectUser()
+        {
+            var pro = db.PROPERTies.ToList();
+            return View(pro);
+        }
+    }
+}
