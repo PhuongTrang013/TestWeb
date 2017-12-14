5c9e53a7443faa1e6f5e5915f5021c12cbd080aa
PhuongTrang <phuongtrang1397@gmail.com>
2017-12-15 00:44:07 +0700

    View List Test

---
+++ PPCRental.AcceptanceTests/Driver/Property/ViewprojectDriver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using PPCRental.Models;
+using PPCRental.AcceptanceTests.Common;
+using PPCRental.Controllers;
+using TechTalk.SpecFlow;
+using PPCRental.AcceptanceTests.Support;
+
+namespace PPCRental.AcceptanceTests.Driver.ViewProjects
+{
+    public class ViewprojectDriver
+    {
+        private ActionResult _result;
+
+        //public void Navigate()
+        //{
+        //    using (var controller = new AccountController())
+        //    {
+        //        _result = controller.Index();
+        //    }
+        //}
+        public void Click()
+        {
+            using (var cont = new ViewListOfProjectController())
+            {
+                _result = cont.ViewListOfProjectUser();
+            }
+        }
+
+        internal void ShowProjects(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowProjects(Table expectedProjects)
+        => ShowProjects(expectedProjects.Rows.Select(r => r["Title"]));
+
+        public void ShowProjects(IEnumerable<string> expectedTitles)
+        {
+           // Act
+            var showProjects = _result.Model<IEnumerable<PROPERTY>>();
+           // Assert
+            ProjectAssertions.HomeScreenShouldShow(showProjects, expectedTitles);
+        }
+
+        internal void ShowProject(object expectedProjects)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PPCRental.AcceptanceTests/Step/ViewListStep.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using PPCRental;
+using PPCRental.AcceptanceTests.Driver.Home;
+using PPCRental.AcceptanceTests.Driver.ViewProjects;
+using PPCRental.UITests.Selenium.Support;
+
+
+namespace PPCRental.AcceptanceTests.Step
+{
+    [Binding, Scope(Tag = "web")]
+    class ViewListStep
+    {
+        private readonly HomeDriver _homdriver;
+        private readonly ViewprojectDriver _viewprojectdriver;
+
+        [Given(@"I am at home page")]
+        public void GivenIAmAtHomePage(Table table)
+        {
+            _homdriver.Navigate();
+        }
+
+        [When(@"I click DUAN")]
+        public void WhenIClickDUAN()
+        {
+            _viewprojectdriver.Click();
+        }
+
+        [Then(@"I will see list of project")]
+        public void ThenIWillSeeListOfProject()
+        {
+            
+            _viewprojectdriver.ShowProjects(expectedProjects.Rows.Select(r => r["Title"]));
+        }
+
+    }
+}
